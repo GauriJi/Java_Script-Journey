a4c758829ef31d6937f0c57d6a4bcaac34a47bd9
Gaurav Bhandari <gauravbhandari367@gmail.com>
2025-12-26 12:35:17 +0530

    Day2

---
+++ day2/conditionalStatements.js
@@ -0,0 +1,24 @@
+let age = 35;
+
+if (age >=45){
+    console.log("You are adult")//If this works(true) every other condition dont 
+}
+else if (age >=18){
+    console.log("You can vote")
+}else{
+    console.log("You cannot vote")
+}
+
+//Ternary Operator
+
+let age1 = 16;
+
+age1>=18 ? console.log("You can vote ") : console.log("You cannot vote");
+
+//or
+
+let agee=55;
+
+let result = agee >=24 ? 'Yes':'No'
+
+console.log(result)
